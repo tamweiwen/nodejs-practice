81532e45095964006defc4532f219bc1c816bbe1
tamweiwen <20170012034@my.xu.edu.ph>
2022-10-07 13:52:02 +0800

    third commit

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-console.log("started a first task");
-
-//check file path
-readFile("./content/first.txt/", "utf8", (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `THIS IS AWESOME : ${first} ${second}`,
+      { flag: "a" }
+    );
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
   }
-  console.log(result);
-  console.log("complete first task");
-});
-console.log("starting next task");
+};
+
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
